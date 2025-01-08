e6de6193be7be68fe26d596e6d3f9387e7972b9c
Muhammad Bilal <149602572+bilal77511@users.noreply.github.com>
2025-01-08 21:41:52 +0500

    Time: 104 ms (12.74%), Space: 19.1 MB (69.83%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,15 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         pink=0
-        if len(nums)==1:
-            return nums[0]
-        for i in range(len(nums)):
-            pink=nums[-1]
-            print(nums.pop())
-            if pink in nums:
-                nums.remove(pink)
-            else:
-                return pink
+        for i in nums:
+            pink = pink^i
+        return pink
 
 
         
